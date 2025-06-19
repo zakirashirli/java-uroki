417122f0cca6c183a1b83a64d61a9273e7754548
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-06-19 21:45:08 +0400

    First Project Created

---
+++ java_uroki_project/src/urokijava/Main.java
@@ -0,0 +1,13 @@
+package urokijava;
+
+public class Main {
+    public static void main(String[] args) {
+        // sysoutprt proydeno
+        /*Its our
+         First
+         java
+         Program
+        */
+        System.out.println("Hello Suel!");
+    }
+}
