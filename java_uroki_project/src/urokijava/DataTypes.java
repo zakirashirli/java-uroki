990d9e003ca46d8f03ed332085dc57ce0ca7575d
zakirashirli <117630695+zakirashirli@users.noreply.github.com>
2025-06-19 22:29:16 +0400

    Urok 1-3

---
+++ java_uroki_project/src/urokijava/DataTypes.java
@@ -0,0 +1,30 @@
+package urokijava;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // целые числа
+        byte my_byte = -20;
+        short my_short = 15000;
+        int my_int = 24000;
+        long my_long = 1000000;
+
+        // дестечные числа
+        float my_float = 10.4f;
+        double my_double = 10.4;
+
+        // char
+        char my_char = '$';
+
+        // boolean
+        boolean my_bool = false;
+
+        // String
+        String my_str = "Hello world!";
+
+        System.out.println(my_byte);
+        System.out.println(my_short);
+        System.out.println(my_int);
+        System.out.println(my_long);
+
+    }
+}
